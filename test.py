858f260f757a2a03e81d08251f2034b4050cc4a1
[Alexandr] <[afominyh242@gmail.com]>
2021-03-13 19:12:21 +0600

    lesson 2 homework

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-test_number = [1234, 5555, 333]  #must to see 10, 15, 9
-
-for number in test_number:
-    number_sum = 0
-    while number:
-        number_sum += number % 10
-        number = number // 10
-    print(number_sum)
+test_string = 'Hello, I am mine.py!'
+test_string_for_question = test_string
+print('f'f'Id of var: {id(test_string)}')
+test_string_split = test_string.split()
+print(f'Methods for str: ', dir(test_string))
+# print('f'f'Id of var: {id(test_string_for_question)}')
